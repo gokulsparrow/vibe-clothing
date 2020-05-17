7e7951f39d1e964ed2e22070722d1ae88a5ea330
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2020-05-17 11:03:23 +0530

    components broken to parts

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 import "./menuitemstyles.scss";
 
-const Menuitem = ({title}) => (
-    <div>
-        <div className='menu-items'>
-            <div className='content'>
-                <h1 className='title'>{title}</h1>
-                <span className='subtitle'>Shop now</span>
-            </div>
-        </div>
+const Menuitem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-);
-
-export default Menuitem;
+  );
+  
+  export default Menuitem;
