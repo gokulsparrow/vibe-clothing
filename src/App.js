1f56a865caa49c4e0f440111177aa056c614f875
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2020-05-20 18:24:34 +0530

    shop page and header added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Homepage from './pages/homepage/Homepage';
-import ShopPage from './pages/shop/shop.component'
-import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
+import { Switch,Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -9,8 +10,11 @@ import './App.css';
 function App () {
     return (
       <div>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
       </div>
     );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/vimeo.svg';
+import './header.style.scss';
+
+const HeaderComponend = () => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo'/>
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                <h3>SHOP</h3>
+            </Link>
+            <Link className='option' to='/contact'>
+                <h3>CONTACT</h3>
+            </Link>
+        </div>
+    </div>
+);
+
+export default HeaderComponend;
