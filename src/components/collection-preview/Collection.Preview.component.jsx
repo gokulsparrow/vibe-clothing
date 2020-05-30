fccb019fdc607752d1975982f972b15afba0106f
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2020-05-30 12:37:44 +0530

    cart utlity method added

--- src/components/collection-preview/Collection.Preview.component.jsx
+++ src/components/collection-preview/Collection.Preview.component.jsx
@@ -6,13 +6,13 @@ import './CollectionPreview-styles.scss';
 
 const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
-        <h1>{title.toUpperCase()}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
-            .filter((items, idx) => idx < 4)
-            .map(({id, ...otherItemProps}) => (
-                <CollectionItem key={id} {...otherItemProps} />
-            ))}
+                .filter((item, idx) => idx < 4)
+                .map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
         </div>
     </div>
 
