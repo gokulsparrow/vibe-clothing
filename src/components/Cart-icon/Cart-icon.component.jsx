a3d02b4c737d35dbaf0e4d0dbc3dedd307ef55df
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2020-05-30 20:06:49 +0530

    selectors added in it

--- src/components/Cart-icon/Cart-icon.component.jsx
+++ src/components/Cart-icon/Cart-icon.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ReactComponent as  ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 import './Cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.action';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -15,4 +16,8 @@ const mapDiaspatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDiaspatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, mapDiaspatchToProps)(CartIcon);
--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cartitem.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 import './cart-dropdown.scss';
 
@@ -18,8 +19,8 @@ const CartDropDown = ({ cartItems }) => (
     </div>
 );
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropDown);
