8678cd14b9314644b547c554f92c233217921de9
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2020-06-01 15:20:13 +0530

    add and remove feature added in cart

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    CLEAR_ITEM_FROM_CART: 'CLAR_ITEM_FROM_CART'
 };
 
 export default CartActionTypes;
